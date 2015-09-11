d92811b3472bfb8a48503e295fb0143f2c7a590b
Tim-TV <admin@Admins-iMac.local>
2015-09-12 00:43:06 +0300

    6th version with independents wdgts

--- 6/wdgt/wdgt2/comp2.js
+++ 6/wdgt/wdgt2/comp2.js
@@ -10,38 +10,38 @@ var Wdgt2 = React.createClass({
             React.createElement(
                 "div",
                 { id: "wrap" },
-                React.createElement("img", { id: "finger", src: "wdgt/34/img/finger.png" }),
+                React.createElement("img", { id: "finger", src: "img/wdgt2/finger.png" }),
                 React.createElement("div", { id: "sleeve" })
             ),
             React.createElement("div", { className: "loader" }),
             React.createElement("div", { className: "loader" }),
             React.createElement("div", { className: "loader" }),
             React.createElement("div", { className: "loader" }),
-            React.createElement("img", { className: "cHouse", id: "c", src: "wdgt/34/img/c.png" }),
+            React.createElement("img", { className: "cHouse", id: "c", src: "img/wdgt2/c.png" }),
             React.createElement(
                 "div",
                 { className: "txt", id: "txt" },
                 "ПРЕДПРИЯТИЕ"
             ),
-            React.createElement("img", { className: "st", id: "st", src: "wdgt/34/img/st.png" }),
+            React.createElement("img", { className: "st", id: "st", src: "img/wdgt2/st.png" }),
             React.createElement(
                 "div",
                 { className: "txt", id: "txt-1" },
                 "отгрузка товара"
             ),
-            React.createElement("img", { className: "cHouse", id: "c-1", src: "wdgt/34/img/c-1.png" }),
+            React.createElement("img", { className: "cHouse", id: "c-1", src: "img/wdgt2/c-1.png" }),
             React.createElement(
                 "div",
                 { className: "txt", id: "txt-2" },
                 "МАГАЗИН"
             ),
-            React.createElement("img", { className: "st", id: "st-1", src: "wdgt/34/img/st-1.png" }),
+            React.createElement("img", { className: "st", id: "st-1", src: "img/wdgt2/st-1.png" }),
             React.createElement(
                 "div",
                 { className: "txt", id: "txt-3" },
                 "оплата товара"
             ),
-            React.createElement("img", { className: "cHouse", id: "c-2", src: "wdgt/34/img/c-2.png" }),
+            React.createElement("img", { className: "cHouse", id: "c-2", src: "img/wdgt2/c-2.png" }),
             React.createElement(
                 "div",
                 { className: "txt", id: "txt-4" },
@@ -51,7 +51,7 @@ var Wdgt2 = React.createClass({
                     "БАНК"
                 )
             ),
-            React.createElement("img", { className: "st", id: "st-2", src: "wdgt/34/img/st-2.png" }),
+            React.createElement("img", { className: "st", id: "st-2", src: "img/wdgt2/st-2.png" }),
             React.createElement(
                 "div",
                 { className: "txt", id: "txt-5" },
@@ -68,7 +68,7 @@ var Wdgt2 = React.createClass({
                 React.createElement("br", null),
                 "в магазин)"
             ),
-            React.createElement("img", { className: "st", id: "st-3", src: "wdgt/34/img/st-3.png" }),
+            React.createElement("img", { className: "st", id: "st-3", src: "img/wdgt2/st-3.png" }),
             React.createElement(
                 "div",
                 { className: "txt", id: "txt-7" },
@@ -85,7 +85,7 @@ var Wdgt2 = React.createClass({
                 React.createElement("br", null),
                 "(0-120 дней)"
             ),
-            React.createElement("img", { className: "circle", id: "circle", src: "wdgt/34/img/circle.png" }),
+            React.createElement("img", { className: "circle", id: "circle", src: "img/wdgt2/circle.png" }),
             React.createElement(
                 "div",
                 { className: "txt", id: "txt-9" },
