016cbaf339456d9cef001d4e3f91a0f313982ef4
Tim-TV <admin@MacBook-Air-Admin-2.local>
2015-11-13 00:22:10 +0300

    pack

--- 7/webpack.config.js
+++ 7/webpack.config.js
@@ -1,7 +1,6 @@
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 var path = require('path');
-var postcss = require('postcss');
 var CompressionPlugin = require("compression-webpack-plugin");
   
 
@@ -19,14 +18,16 @@ module.exports = {
       { test: /\.png$/, loader: "url-loader?limit=100000" },
       { test: /\.jpg$/, loader: "file-loader" },
       { test: /\.svg$/, loader: "url-loader?limit=100000" },
-      { test: /\.js[x]?$/, exclude: /node_modules/, loader: 'babel-loader?presets[]=es2015&presets[]=react' },
+      { test: /\.js[x]?$/, exclude: /node_modules/, loader: 'babel-loader',
+        query: {
+            presets: ['es2015', 'react']
+          }
+        },
+
       { test: /\.scss$/, loader: ExtractTextPlugin.extract("style-loader", "css-loader?module!autoprefixer-loader!sass-loader")},
       { test: /\.css$/, loader: ExtractTextPlugin.extract("style-loader", "css-loader?module!cssnext-loader")}
     ]
   },
-
-  // Provide the Local Scope plugin to postcss-loader:
-  postcss: [ require('postcss-local-scope')],
 
   plugins: [
     new ExtractTextPlugin('style.css', { allChunks: true }),
