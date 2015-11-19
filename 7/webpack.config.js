19acc7bb1b15c4dfc70b7b59b71747e2519f398d
Tim-TV <admin@MacBook-Air-Admin-2.local>
2015-11-19 18:21:21 +0300

    0.0.03.4

--- 7/webpack.config.js
+++ 7/webpack.config.js
@@ -42,36 +42,3 @@ module.exports = {
   watch: true,
   devtool: "source-map"
 };
-/*
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
-
-var path = require('path');
-var postcss = require('postcss');
-  
-
-module.exports = {
-  entry: './src/index.js',
-
-  output: {
-    filename: 'index.js',
-    path: path.resolve('./build'),
-    libraryTarget: 'umd'
-  }, 
-
-  module: {
-    loaders: [
-      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
-      { test: /\.scss$/, loaders: ["style", "css", "sass"] },
-      { test: /\.css$/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader?localIdentName=[name]__[local]___[hash:base64:5]!postcss-loader') }
-    ]
-  },
-
-  // Provide the Local Scope plugin to postcss-loader:
-  postcss: [ require('postcss-local-scope')],
-  
-
-  plugins: [
-    new ExtractTextPlugin('style.css', { allChunks: true })
-  ]
-};
-*/
